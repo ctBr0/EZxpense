81aa5093da808af2ccf9e7e81334d78b71d8fb9f
ctBr0 <justin6008@icloud.com>
2023-07-06 16:09:03 -0700

    more

---
+++ src/app/pages/intro/intro.page.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
+@Component({
+  selector: 'app-intro',
+  templateUrl: './intro.page.html',
+  styleUrls: ['./intro.page.scss'],
+  standalone: true,
+  imports: [IonicModule, CommonModule, FormsModule],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+})
+export class IntroPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
