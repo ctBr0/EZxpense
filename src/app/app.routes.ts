17632d5a125f8324497f31e6645425699f2e8261
ctBr0 <justin6008@icloud.com>
2023-07-11 22:48:24 -0700

    changed app.routes and main.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,25 +1,30 @@
 import { Routes } from '@angular/router';
+import { redirectUnauthorizedTo, redirectLoggedInTo, canActivate } from '@angular/fire/auth-guard';
 
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: '/login',
+    redirectTo: '/intro',
     pathMatch: 'full'
   },
   {
     path: 'tabs',
-    loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes)
+    loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes),
+    ...canActivate(() => redirectUnauthorizedTo(['']))
   },
   {
     path: 'intro',
-    loadComponent: () => import('./pages/intro/intro.page').then( m => m.IntroPage)
+    loadComponent: () => import('./pages/intro/intro.page').then( m => m.IntroPage),
+    ...canActivate(() => redirectLoggedInTo(['tabs']))
   },
   {
     path: 'login',
-    loadComponent: () => import('./pages/login/login.page').then( m => m.LoginPage)
+    loadComponent: () => import('./pages/login/login.page').then( m => m.LoginPage),
+    ...canActivate(() => redirectLoggedInTo(['tabs']))
   },
   {
     path: 'signup',
-    loadComponent: () => import('./pages/signup/signup.page').then( m => m.SignupPage)
+    loadComponent: () => import('./pages/signup/signup.page').then( m => m.SignupPage),
+    ...canActivate(() => redirectLoggedInTo(['tabs']))
   },
 ];
--- src/main.ts
+++ src/main.ts
@@ -7,14 +7,40 @@ import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 import { environment } from './environments/environment';
 
+import { provideFirebaseApp } from '@angular/fire/app';
+import { initializeApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getStorage, provideStorage } from '@angular/fire/storage';
+import { getAuth, provideAuth} from '@angular/fire/auth';
+import { Capacitor } from '@capacitor/core';
+import { indexedDBLocalPersistence, initializeAuth } from 'firebase/auth';
+import { getApp } from 'firebase/app';
+import { defineCustomElements } from '@ionic/pwa-elements/loader';
+
 if (environment.production) {
   enableProdMode();
 }
 
+defineCustomElements(window);
+
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-    importProvidersFrom(IonicModule.forRoot({})),
+    importProvidersFrom(
+      IonicModule.forRoot({}),
+      provideFirebaseApp(() => initializeApp(environment.firebase)),
+      provideFirestore(() => getFirestore()),
+      provideStorage(() => getStorage()),
+      provideAuth(() => {
+        if (Capacitor.isNativePlatform()) {
+          return initializeAuth(getApp(), {
+            persistence: indexedDBLocalPersistence
+          });
+        } else {
+          return getAuth();
+        }
+      }),
+    ),
     provideRouter(routes),
   ],
 });
